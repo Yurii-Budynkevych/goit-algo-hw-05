645b2143eeda3abab1282abe73592d08c003da15
Yurii Budynkevych <yurii.budynkevych@gmail.com>
2024-02-11 22:13:39 +0200

    task4 add

--- assistant01.py
+++ assistant01.py
@@ -1,18 +1,36 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Please enter name and phone."
+        except KeyError:
+            return "Please enter existing contact."
+        except IndexError:
+            return "Please enter existing contact."
+    return inner
+
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
+@input_error
 def add_contact(args, dict):
     name, phone = args
     dict[name] = phone
     return "Contact added."
 
+@input_error
 def change_contact(args, dict):
     name, phone = args
+    value = dict.get(name)
+    if value is None:
+        raise KeyError()
     dict[name] = phone
     return "Contact changed."
 
+@input_error
 def show_phone(contact, dict):
     return dict[contact[0]]
 
